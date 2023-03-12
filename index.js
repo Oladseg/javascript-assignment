2b7c0659578aa9f02f132042ef06f65150d0f30c
Okunade Adeola <adeolafunke7@gmail.com>
2023-03-12 19:12:42 +0100

    updated

--- index.js
+++
@@ -1,8 +0,0 @@
-prompt("enter your pin");
-const balance = 1000000
-let amount=prompt("enter amount to be withdrawn");
-if(amount == balance){
-    if( amount < 90000)
-    console.log("please wait while we process your request");
-    else
-    console.log("insuicient fund")};
